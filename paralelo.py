aff394a9f5d59220d64a3b898a562872d631c930
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-02-13 17:09:57 +0100

    añadido el guardado

--- paralelo.py
+++ paralelo.py
@@ -4,6 +4,7 @@ from time import sleep  # importo la función sleep de la libreria time¡
 from multiprocessing import Pool # importo la clase Pool de la librería multiprocessing
 from introducir.numero import solicitar_introducir_numero_extremo # importo la función solicitar_introducir_numero_extremo del módulo introducir.numero
 from colorama import Fore  # importo la librería colorama
+import os # importo la librería os
 import pathlib # importo la librería pathlib
 actual_path = pathlib.Path(__file__).parent.absolute() # guardo la ruta absoluta del archivo en la variable actual_path
 
@@ -34,7 +35,7 @@ if __name__ == "__main__":
         lista1 = []
         lista2 = []
     def main(): # función main
-        eleccion = solicitar_introducir_numero_extremo(Fore.CYAN +"¿Quieres ejecutar el programa en paralelo? 1: No, 2: Si, 3: Ver medias de tiempo de ejecución, 4: Salir", 1, 3) ; print(Fore.RESET) # solicito al usuario que introduzca un número entre 1 y 3
+        eleccion = solicitar_introducir_numero_extremo(Fore.CYAN +"¿Quieres ejecutar el programa en paralelo? 1: No, 2: Si, 3: Ver medias de tiempo de ejecución, 4: Guardar y Salir", 1, 3) ; print(Fore.RESET) # solicito al usuario que introduzca un número entre 1 y 3
 
         if eleccion == 1:  # main del programa sin paralelizar
             inicio = time.time() # guardo el tiempo en el que empieza el programa
@@ -69,6 +70,9 @@ if __name__ == "__main__":
             print("La media de tiempo de ejecución sin paralelizar es: ", sum(lista1) / len(lista1))
             print("La media de tiempo de ejecución en paralelo es: ", sum(lista2) / len(lista2))
         elif eleccion == 4:
+            os.system('git add .')
+            os.system('git commit -m "Actualización de tiempos"')
+            os.system('git push')
             exit() # salgo del programa
     main()
 
